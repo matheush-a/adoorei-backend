0aaf0209b4215599622ef7bdfce968675f3b7c91
matheush-a <mathzalmeida@gmail.com>
2024-03-01 11:45:46 -0300

    chore: added is_active status column to sales table

--- database/migrations/2024_03_01_140815_create_sales_table.php
+++ database/migrations/2024_03_01_140815_create_sales_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('sales', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('amount');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
